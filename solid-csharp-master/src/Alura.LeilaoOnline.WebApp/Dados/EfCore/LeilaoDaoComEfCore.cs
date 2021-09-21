528d4c917ae8c6db852d2c2e9069f2901959ea8b
Marcos Junior <Mod.gluguer@gmail.com>
2021-09-20 23:15:31 -0300

    inversao de dependencia

---
+++ solid-csharp-master/src/Alura.LeilaoOnline.WebApp/Dados/EfCore/LeilaoDaoComEfCore.cs
@@ -0,0 +1,49 @@
+﻿using Alura.LeilaoOnline.WebApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alura.LeilaoOnline.WebApp.Dados.EfCore
+{
+    public class LeilaoDaoComEfCore : ILeilaoDao
+    {
+        private readonly AppDbContext _context;
+
+        public LeilaoDaoComEfCore()
+        {
+            _context = new AppDbContext();
+        }
+        public IEnumerable<Categoria> BurcarCategorias()
+        {
+            return _context.Categorias.ToList();
+        }
+
+        public IEnumerable<Leilao> BuscarLeiloes()
+        {
+            return _context.Leiloes.Include(c => c.Categoria).ToList();
+        }
+        public Leilao BuscarPorID(int id)
+        {
+            return _context.Leiloes.First(c => c.Id == id);
+        }
+
+        public void Incluir(Leilao leilao)
+        {
+            _context.Leiloes.Add(leilao);
+            _context.SaveChanges();
+        }
+
+        public void Alterar(Leilao leilao)
+        {
+            _context.Leiloes.Update(leilao);
+            _context.SaveChanges();
+        }
+        public void Excluir(Leilao leilao)
+        {
+            _context.Leiloes.Remove(leilao);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ solid-csharp-master/src/Alura.LeilaoOnline.WebApp/Dados/ILeilaoDao.cs
@@ -0,0 +1,15 @@
+﻿using Alura.LeilaoOnline.WebApp.Models;
+using System.Collections.Generic;
+
+namespace Alura.LeilaoOnline.WebApp.Dados
+{
+    public interface ILeilaoDao
+    {
+        IEnumerable<Categoria> BurcarCategorias();
+        IEnumerable<Leilao> BuscarLeiloes();
+        Leilao BuscarPorID(int id);
+        void Incluir(Leilao leilao);
+        void Alterar(Leilao leilao);
+        public void Excluir(Leilao leilao);
+    }
+}
