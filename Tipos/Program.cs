672ce067d4d4554e137241078eb69b4eb893b13e
Marcos Junior <Mod.gluguer@gmail.com>
2022-06-17 15:52:56 -0300

    principios solid

---
+++ Tipos/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tipos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 6_00____________________________________0_000;
+        }
+    }
+}
