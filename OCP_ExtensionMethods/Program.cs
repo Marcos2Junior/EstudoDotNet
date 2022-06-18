16e42e78a44db0c26cfeb12b8c9e3fc816f5d906
Marcos Junior <Mod.gluguer@gmail.com>
2022-06-18 18:25:02 -0300

    ocp extension

---
+++ OCP_ExtensionMethods/Program.cs
@@ -0,0 +1,16 @@
+﻿using OCP_ExtensionMethods;
+using OCP_ExtensionMethods.Extensions;
+
+Produto produto1 = new Produto(
+               1,
+               "Caneta",
+               "Caneta Esferográfica azul",
+               2.00M,
+               3.40M,
+               100,
+               true
+               );
+
+Console.WriteLine($"Produto = {produto1.Nome} Lucro = {produto1.MargemLucro()}");
+
+Console.ReadLine();
